9474bb20ff4f30f33d29efbb29813c433f7231de
Dracocybertech <pointdujourkh@gmail.com>
2025-01-24 18:05:49 +0100

    Init Author

---
+++ src/com/github/gestion_mediatheque/people/Author.java
@@ -0,0 +1,10 @@
+package com.github.gestion_mediatheque.people;
+
+import java.time.LocalDate;
+
+public class Author {
+    private String id;
+    private String name;
+    private LocalDate birthdate;
+    private String nationality;
+}
