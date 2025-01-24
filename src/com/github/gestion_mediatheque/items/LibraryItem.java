398c350da89432de465946de38a2956e90ac32ef
Dracocybertech <pointdujourkh@gmail.com>
2025-01-24 17:51:41 +0100

    +getId

--- src/com/github/gestion_mediatheque/items/LibraryItem.java
+++ src/com/github/gestion_mediatheque/items/LibraryItem.java
@@ -1,5 +1,9 @@
 package com.github.gestion_mediatheque.items;
 
 public interface LibraryItem {
-
+    /**
+     * Return the id of the item.
+     * @return String
+     */
+    public String getId();
 }
