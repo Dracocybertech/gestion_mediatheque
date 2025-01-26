0b6d72d0b2c53abcb4e29551c3de71b86940b30f
Dracocybertech <pointdujourkh@gmail.com>
2025-01-26 19:48:54 +0100

    Refacto exceptions expected

--- test/com/github/gestion_mediatheque/items/BookTest.java
+++ test/com/github/gestion_mediatheque/items/BookTest.java
@@ -9,78 +9,74 @@ import org.junit.Before;
 import org.junit.Test;
 
 import com.github.gestion_mediatheque.items.Book;
-import com.github.gestion_mediatheque.items.NullAuthorException;
-import com.github.gestion_mediatheque.items.NullTitleException;
+import com.github.gestion_mediatheque.items.NullEmptyAttributeException;
 import com.github.gestion_mediatheque.people.Author;
 
 public class BookTest {
-    
+
     Author author1;
     ArrayList<Author> listAuthors;
     String originalId = "id";
     String originalTitle = "title";
 
     Logger logger = Logger.getLogger(getClass().getName());
-    
+
     @Before
-    public void beforeTest(){
+    public void beforeTest() {
         logger.info("Begin Book test");
-        try{
+        try {
             String authorName = "author1";
             author1 = new Author(authorName);
             listAuthors = new ArrayList<>();
             listAuthors.add(author1);
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             logger.severe(e.getMessage());
         }
     }
 
     @Test
-    public void createBookTest(){
+    public void createBookTest() {
         String id = originalId.concat("1");
         String title = originalTitle.concat("1");
         String id2 = originalId.concat("2");
         String title2 = originalTitle.concat("2");
 
-        try{
+        try {
             new Book(id, title, listAuthors);
             new Book(id2, title2, listAuthors);
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             fail(e.getMessage());
         }
     }
 
-    @Test(expected=NullTitleException.class)
-    public void nullTitleException() throws NullTitleException, NullAuthorException{
+    @Test(expected = NullEmptyAttributeException.class)
+    public void nullAttributeException() throws NullEmptyAttributeException {
         String id = originalId.concat("1");
         String nullTitle = null;
         new Book(id, nullTitle, listAuthors);
     }
 
-    @Test(expected=NullTitleException.class)
-    public void emptyTitleException() throws NullTitleException, NullAuthorException{
+    @Test(expected = NullEmptyAttributeException.class)
+    public void emptyTitleException() throws NullEmptyAttributeException {
         String id = originalId.concat("1");
         String nullTitle = "";
         new Book(id, nullTitle, listAuthors);
     }
 
-    @Test(expected=NullAuthorException.class)
-    public void nullAuthorsException() throws NullTitleException, NullAuthorException {
+    @Test(expected = NullEmptyAttributeException.class)
+    public void nullAuthorsException() throws NullEmptyAttributeException {
         String id = originalId.concat("1");
         String title = originalTitle.concat("1");
         listAuthors = null;
         new Book(id, title, listAuthors);
     }
 
-    @Test(expected=NullAuthorException.class)
-    public void emptyAuthorsException() throws NullTitleException, NullAuthorException {
+    @Test(expected = NullEmptyAttributeException.class)
+    public void emptyAuthorsException() throws NullEmptyAttributeException {
         String id = originalId.concat("1");
         String title = originalTitle.concat("1");
         listAuthors.clear();
         new Book(id, title, listAuthors);
     }
-
 
 }
