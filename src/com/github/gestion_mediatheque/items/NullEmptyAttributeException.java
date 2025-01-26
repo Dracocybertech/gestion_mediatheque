2fedbb223b4d3f9973290f030528aca4cd2d0232
Dracocybertech <pointdujourkh@gmail.com>
2025-01-26 19:48:41 +0100

    Add a space in the message

--- src/com/github/gestion_mediatheque/items/NullEmptyAttributeException.java
+++ src/com/github/gestion_mediatheque/items/NullEmptyAttributeException.java
@@ -2,10 +2,11 @@ package com.github.gestion_mediatheque.items;
 
 public class NullEmptyAttributeException extends Exception {
     /**
-     * Exception raised for empty or null attributes at the creation of an object. 
-     * @param attributeName 
+     * Exception raised for empty or null attributes at the creation of an object.
+     * 
+     * @param attributeName
      */
     public NullEmptyAttributeException(String attributeName) {
-        super("The attribute " + attributeName + "can't be null nor empty.");
+        super("The attribute " + attributeName + " can't be null nor empty.");
     }
 }
