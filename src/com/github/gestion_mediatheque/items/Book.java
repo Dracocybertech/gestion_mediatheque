9a644cd2292b857073c6c8bb7ef4b221ec1dfd33
Dracocybertech <pointdujourkh@gmail.com>
2025-01-26 19:46:37 +0100

    Refacto exceptions used in the constructor

--- src/com/github/gestion_mediatheque/items/Book.java
+++ src/com/github/gestion_mediatheque/items/Book.java
@@ -4,20 +4,20 @@ import java.util.List;
 
 import com.github.gestion_mediatheque.people.Author;
 
-public class Book implements LibraryItem{
-    
+public class Book implements LibraryItem {
+
     private String id;
     private String title;
     private List<Author> authors;
 
-    public Book(String id, String title, List<Author> authors) throws NullTitleException, NullAuthorException{
-        //Non empty title
-        if (title == null || title.isEmpty()){
-            throw new NullTitleException("Title cannot be empty. Please add a title with at least one character.");
+    public Book(String id, String title, List<Author> authors) throws NullEmptyAttributeException {
+        // Non empty title
+        if (title == null || title.isEmpty()) {
+            throw new NullEmptyAttributeException("title");
         }
-        //Non empty author
-        if (authors == null || authors.isEmpty()){
-            throw new NullAuthorException("Book needs at least one author to be created.");
+        // Non empty author
+        if (authors == null || authors.isEmpty()) {
+            throw new NullEmptyAttributeException("authors");
         }
 
         this.title = title;
@@ -25,15 +25,15 @@ public class Book implements LibraryItem{
         this.id = id;
     }
 
-    public String getId(){
+    public String getId() {
         return this.id;
     }
 
-    public String getTitle(){
+    public String getTitle() {
         return this.title;
     }
 
-    public List<Author> getAuthors(){
+    public List<Author> getAuthors() {
         return this.authors;
     }
 }
