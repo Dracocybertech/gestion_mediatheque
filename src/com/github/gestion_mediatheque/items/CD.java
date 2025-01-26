f216704e985639644b3bf8764501512b1f5a8861
Dracocybertech <pointdujourkh@gmail.com>
2025-01-26 19:27:41 +0100

    +Constructor

--- src/com/github/gestion_mediatheque/items/CD.java
+++ src/com/github/gestion_mediatheque/items/CD.java
@@ -6,6 +6,37 @@ public class CD implements LibraryItem{
     private String artistName;
     private Integer tracksNumber;
 
+    /**
+     * Create a CD.
+     * 
+     * @param id
+     * @param title
+     * @param artistName
+     * @param tracksNumber
+     * @throws NullEmptyAttributeException
+     * @throws NegativeTracksNumberException
+     */
+    public CD(String id, String title, String artistName, Integer tracksNumber)
+            throws NullEmptyAttributeException, NegativeTracksNumberException {
+        if (id == null || id.isEmpty()) {
+            throw new NullEmptyAttributeException("id");
+        }
+        if (title == null || title.isEmpty()) {
+            throw new NullEmptyAttributeException("title");
+        }
+        if (artistName == null || artistName.isEmpty()) {
+            throw new NullEmptyAttributeException("artistName");
+        }
+        if (tracksNumber == null) {
+            throw new NegativeTracksNumberException();
+        }
+
+        this.id = id;
+        this.title = title;
+        this.artistName = artistName;
+        this.tracksNumber = tracksNumber;
+    }
+
     public String getId(){
         return this.id;
     }
