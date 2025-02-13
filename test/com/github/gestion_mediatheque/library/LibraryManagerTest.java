a3b96b37b28465b20f628f8047a1747ecb7d8e29
Dracocybertech <pointdujourkh@gmail.com>
2025-02-13 18:52:33 +0100

    Init LibraryManagerTest

---
+++ test/com/github/gestion_mediatheque/library/LibraryManagerTest.java
@@ -0,0 +1,16 @@
+package test.com.github.gestion_mediatheque.library;
+
+import java.util.logging.Logger;
+
+import org.junit.Before;
+
+public class LibraryManagerTest {
+
+    Logger logger = Logger.getLogger(getClass().getName());
+
+    @Before
+    public void beforeTest() {
+        logger.info("Begin Library manager test");
+    }
+
+}
